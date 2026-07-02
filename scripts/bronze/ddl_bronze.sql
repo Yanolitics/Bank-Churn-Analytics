5e0114bb263b84cf20ae97cbde2f0bc176522724
Timothy E. <timothyespiritu66@gmail.com>
2026-07-03 00:12:16 +0800

    Modify SQL script for multiple raw landing tables
    Updated the SQL script to create raw landing tables for CRM, CBS, and CFL CSV files, replacing the previous single table for BankChurners. Adjusted table definitions and added necessary checks for table existence.

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -2,7 +2,7 @@
 -- Project: Bank Churn Analytics
 -- Task: Data Warehouse - Bronze Layer Ingestion (BankChurners)
 -- Developer: Yanolitics
--- Purpose: Creates raw landing table for file BankChurner.
+-- Purpose: Creates raw landing tables for CRM, CBS, and The Central Financial Ledger (CFL) CSV files.
 -- WARNING: Running this script drops existing tables and permanently 
 --          deletes all data within them. Dev use only!
 -- ====================================================================
@@ -10,38 +10,57 @@
 USE BankChurnAnalytics;
 GO
 
--- ─── SECTION 1: CRM TABLES ───────────────────────────────────────────
+-- ─── SECTION 1: TABLES ───────────────────────────────────────────
 
 -- 1. Customer Info Table
-IF OBJECT_ID('bronze.bankchurners', 'U') IS NOT NULL
-    DROP TABLE bronze.bankchurners;
+IF OBJECT_ID('bronze.crm_profiles', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_profiles;
 GO
 
-CREATE TABLE bronze.bankchurners (
+CREATE TABLE bronze.crm_profiles (
     CLIENTNUM INT,
-    Attrition_Flag NVARCHAR(50),
     Customer_Age INT,
     Gender NVARCHAR(10),
     Dependent_count INT,
     Education_Level NVARCHAR (20),
     Marital_Status NVARCHAR (20),
-    Income_Category NVARCHAR (20),
+    Contacts_Count_12_mon INT,
+ );
+GO   
+    
+-- 2. Core Banking System
+IF OBJECT_ID('bronze.credit_accounts_ops', 'U') IS NOT NULL
+    DROP TABLE bronze.credit_accounts_ops;
+GO
+
+CREATE TABLE bronze.credit_accounts_ops (
+    CLIENTNUM INT,
+    Attrition_Flag NVARCHAR(50),
     Card_Category NVARCHAR (20),
+    Income_Category NVARCHAR (20),
     Months_on_book INT,
     Total_Relationship_Count INT,
-    Months_Inactive_12_mon INT,
-    Contacts_Count_12_mon INT,
+    Months_Inactive_12_mon INT
+ );
+GO    
+
+-- 3. The Central Financial Ledger
+IF OBJECT_ID('bronze.ledger_transactions', 'U') IS NOT NULL
+    DROP TABLE bronze.ledger_transactions;
+GO
+
+CREATE TABLE bronze.ledger_transactions (
+    CLIENTNUM INT,
     Credit_Limit FLOAT,
     Total_Revolving_Bal INT,
     Avg_Open_To_Buy FLOAT,
-    Total_Amt_Chng_Q4_Q1 FLOAT,
     Total_Trans_Amt INT,
     Total_Trans_Ct INT,
-    Total_Ct_Chng_Q4_Q1 FLOAT,
-    Avg_Utilization_Ratio FLOAT
-);
+    Avg_Utilization_Ratio FLOAT,
+    Total_Amt_Chng_Q4_Q1 FLOAT,
+    Total_Ct_Chng_Q4_Q1 FLOAT
+ );
 GO
-
 
 -- ─── SECTION 2: VERIFICATION AUDIT ──────────────────────────────────
 
